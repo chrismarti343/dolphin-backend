c9e3d2b1a5471b9b232365cc1a2a87a2d9c75af9
Lakeram Narine <nar19002@byui.edu>
2021-09-29 18:43:15 -0600

    backend

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from "express";
 const app = express();
 
 const port = 5000;
-app.listen(port,()=>{
-    console.log("Listening on port", port)
+app.listen(port, () => {
+    console.log("Listeeeening on port", port)
 })
